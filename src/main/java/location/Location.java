982514cdb13c6f7bd3e9ca60ea2ce24125de202a
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-04-04 20:19:10 +0200

    Location - capital mapping

--- src/main/java/location/Location.java
+++ src/main/java/location/Location.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableBiMap;
 import java.util.Optional;
 
 import static external.Country.*;
+import static location.Capital.*;
 import static location.Country.*;
 
 /**
@@ -16,6 +17,12 @@ class Location {
             GERMANY, DE,
             JAPAN, JPN
     );
+
+    private final static ImmutableBiMap<Capital, String> CAPITAL_MAPPING = ImmutableBiMap.of(
+            BUENOS_AIRES, "buenos-aires",
+            MEXICO_CITY, "mexico city",
+            WASHINGTON, "washington, dc"
+    );
     
     static Optional<Country> toDomainCountry(external.Country countryExternal) {
         return Optional.ofNullable(COUNTRY_MAPPING.get(countryExternal));
@@ -23,6 +30,14 @@ class Location {
     
     static Optional<external.Country> toExternalCountry(Country countryInternal) {
         return Optional.ofNullable(COUNTRY_MAPPING.inverse().get(countryInternal));
+    }
+    
+    static Optional<Capital> toDomainCapital(String capitalExternal) {
+        return Optional.ofNullable(CAPITAL_MAPPING.inverse().get(capitalExternal));
+    }
+    
+    static Optional<String> toExternalCapital(Capital capital) {
+        return Optional.ofNullable(CAPITAL_MAPPING.get(capital));
     }
 }
 
--- src/test/java/location/LocationTest.java
+++ src/test/java/location/LocationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static external.Country.*;
+import static location.Capital.*;
 import static location.Country.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -26,5 +27,20 @@ public class LocationTest {
         assertThat(Optional.of(POLAND), is(Location.toExternalCountry(PL)));
         assertThat(Optional.of(GERMANY), is(Location.toExternalCountry(DE)));
         assertThat(Optional.of(JAPAN), is(Location.toExternalCountry(JPN)));
+    }
+
+
+    @Test
+    public void toDomainCapital() {
+        assertThat(Optional.of(BUENOS_AIRES), is(Location.toDomainCapital("buenos-aires")));
+        assertThat(Optional.of(MEXICO_CITY), is(Location.toDomainCapital("mexico city")));
+        assertThat(Optional.of(WASHINGTON), is(Location.toDomainCapital("washington, dc")));
+    }
+
+    @Test
+    public void toExternalCapital() {
+        assertThat(Optional.of("buenos-aires"), is(Location.toExternalCapital(BUENOS_AIRES)));
+        assertThat(Optional.of("mexico city"), is(Location.toExternalCapital(MEXICO_CITY)));
+        assertThat(Optional.of("washington, dc"), is(Location.toExternalCapital(WASHINGTON)));
     }
 }
